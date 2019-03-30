5c407f27b2562c94a9158841906bd459c03e81e0
Yukino Hayakawa <tennencoll@gmail.com>
2019-03-30 18:00:27 +0800

    add files for layout code based on genetic algorithm

---
+++ Genetic/GeneticOptimizer.hpp
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <vector>
+
+namespace usagi
+{
+template <
+    typename Genetype,
+    typename GenetypeTraits,
+    typename
+>
+struct GeneticOptimizer
+{
+    std::vector<Genetype> population;
+
+    void step()
+    {
+    }
+
+    void optimize()
+    {
+        initPopulation();
+        do
+        {
+            for(auto &&p : population)
+            {
+                chooseParents();
+                crossover();
+                mutate();
+
+            }
+        }
+        while(!terminateCondition());
+    }
+};
+}
