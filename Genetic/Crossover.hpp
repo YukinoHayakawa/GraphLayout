e7ef2d41393803d425212c731d4f155540c240d6
Yukino Hayakawa <tennencoll@gmail.com>
2019-04-02 18:46:02 +0800

    one point crossover operator

---
+++ Genetic/Crossover.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <random>
+#include <cassert>
+#include <algorithm>
+
+namespace usagi::genetic::crossover
+{
+// suppose the genotype is a linear container like vector/string
+struct OnePointCrossover
+{
+    template <typename Genotype, typename RNG>
+    void operator()(Genotype &a, Genotype &b, RNG &rng)
+    {
+        assert(a.size() == b.size());
+        // since int distribution is closed range, it's possible that the
+        // two genotypes are just swapped.
+        std::uniform_int_distribution<typename Genotype::size_type> dist(
+            0, a.size()
+        );
+        auto split_point = dist(rng);
+        // aaaaaaaaaaaaaaaa --> bbbbbbaaaaaaaaaa
+        // bbbbbbbbbbbbbbbb --> aaaaaabbbbbbbbbb
+        std::swap_ranges(a.begin(), a.begin() + split_point, b.begin());
+    }
+};
+}
