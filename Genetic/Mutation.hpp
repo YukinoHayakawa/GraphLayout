aa90d3c82ca31ec12e8aaf12abfb0ae4a6bdef9a
Yukino Hayakawa <tennencoll@gmail.com>
2019-04-03 21:53:35 +0800

    replaceworst & uniformmutation

--- Genetic/Mutation.hpp
+++ Genetic/Mutation.hpp
@@ -6,8 +6,7 @@
 // Mutation occurs during evolution according to a user-definable mutation probability. This probability should be set low. If it is set too high, the search will turn into a primitive random search.
 namespace usagi::genetic::mutation
 {
-// add a Gaussian distributed number to one gene
-
+// add a Gaussian distributed number to one
 template <
     typename Genotype
 >
@@ -27,6 +26,29 @@ struct GaussianMutation
         auto gene_pos = gene_dist(rng);
 
         std::normal_distribution<ValueT> num_dist(mean, std_dev);
+        a[gene_pos] += num_dist(rng);
+    }
+};
+
+template <
+    typename Genotype
+>
+struct UniformMutation
+{
+    using ValueT = typename Genotype::value_type;
+    ValueT min = -1;
+    ValueT max = 1;
+
+    template <typename RNG>
+    void operator()(Genotype &a, RNG &rng)
+    {
+        assert(a.size() > 0);
+        std::uniform_int_distribution<typename Genotype::size_type> gene_dist(
+            0, a.size() - 1
+        );
+        auto gene_pos = gene_dist(rng);
+
+        std::uniform_real_distribution<ValueT> num_dist(min, max);
         a[gene_pos] += num_dist(rng);
     }
 };
--- Genetic/Replacement.hpp
+++ Genetic/Replacement.hpp
@@ -4,7 +4,7 @@
 
 namespace usagi::genetic::replacement
 {
-struct RemoveOldestReplacementPolicy
+struct ReplaceOldest
 {
 	template <
 		typename Optimizer,
@@ -22,6 +22,27 @@ struct RemoveOldestReplacementPolicy
 				oldest = &individual;
 		}
 		return *oldest;
+	}
+};
+
+struct ReplaceWorst
+{
+	template <
+		typename Optimizer,
+		typename Individual = typename Optimizer::IndividualT
+	>
+	auto operator()(Optimizer &o, Individual *other) -> Individual &
+	{
+		assert(o.population.size() > 1);
+		Individual *worst = nullptr;
+		// todo priority queue
+		for(auto &&individual : o.population)
+		{
+			if((worst == nullptr || individual.fitness < worst->fitness)
+				&& &individual != other)
+				worst = &individual;
+		}
+		return *worst;
 	}
 };
 }
