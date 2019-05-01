a0eb920cea8cc6de531305697a49b8177ae05865
Yukino Hayakawa <tennencoll@gmail.com>
2019-05-01 12:25:34 +0800

    move impl to body file

--- Graph/NodeGraph.cpp
+++ Graph/NodeGraph.cpp
@@ -4,6 +4,109 @@
 
 #include <Usagi/Core/Logging.hpp>
 
+usagi::node_graph::Port::Port(std::string name, Edge edge, float edge_pos)
+	: name(std::move(name))
+	, edge(edge)
+	, edge_pos(edge_pos)
+{
+}
+
+usagi::node_graph::NodePrototype::NodePrototype(std::string name, Vector2f size)
+	: name(std::move(name))
+	, size(std::move(size))
+{
+}
+
+usagi::node_graph::NodePrototype::NodePrototype(
+	std::string name,
+	Vector2f size,
+	std::size_t in,
+	std::size_t out)
+	: NodePrototype(name, size)
+{
+	createInPorts(in);
+	createOutPorts(out);
+}
+
+void usagi::node_graph::NodePrototype::createPorts(
+	std::vector<Port> &sink,
+	std::size_t amount,
+	Port::Edge edge)
+{
+	sink.resize(amount);
+	const float step = 1.f / (amount + 1);
+	for(std::size_t i = 0; i < amount; ++i)
+	{
+		sink[i].edge = edge;
+		sink[i].edge_pos = step * (i + 1);
+	}
+}
+
+usagi::Vector2f usagi::node_graph::NodePrototype::portPosition(
+	const Port &p,
+	const Vector2f &position) const
+{
+	Vector2f pos;
+	switch(p.edge)
+	{
+		case Port::Edge::NORTH:
+			pos.x() = lerp(p.edge_pos, 0.f, size.x())
+				+ position.x();
+			pos.y() = position.y();
+			break;
+		case Port::Edge::SOUTH:
+			pos.x() = lerp(p.edge_pos, 0.f, size.x())
+				+ position.x();
+			pos.y() = position.y() + size.y();
+			break;
+		case Port::Edge::WEST:
+			pos.x() = position.x();
+			pos.y() = lerp(p.edge_pos, 0.f, size.y())
+				+ position.y();
+			break;
+		case Port::Edge::EAST:
+			pos.x() = position.x() + size.x();
+			pos.y() = lerp(p.edge_pos, 0.f, size.y())
+				+ position.y();
+			break;
+		default: ;
+	}
+	return pos;
+}
+
+usagi::node_graph::Node::Node(NodePrototype *prototype, std::string name)
+	: prototype(prototype)
+	, name(std::move(name))
+{
+}
+
+usagi::node_graph::Link::Link(
+	std::size_t node0,
+	std::size_t port0,
+	std::size_t node1,
+	std::size_t port1)
+	: node0(node0)
+	, port0(port0)
+	, node1(node1)
+	, port1(port1)
+{
+}
+
+std::tuple<const usagi::node_graph::Node &, const usagi::node_graph::Port &,
+	const usagi::node_graph::Node &, const usagi::node_graph::Port &> usagi::
+node_graph::NodeGraph::mapLink(std::size_t i) const
+{
+	auto &l = link(i);
+	auto &n0 = node(l.node0);
+	auto &n1 = node(l.node1);
+	auto &p0 = n0.prototype->outPort(l.port0);
+	auto &p1 = n1.prototype->inPort(l.port1);
+	return std::make_tuple(
+		std::ref(n0), std::ref(p0),
+		std::ref(n1), std::ref(p1)
+	);
+}
+
 usagi::node_graph::NodeGraph usagi::node_graph::NodeGraph::readFromFile(
 	const std::filesystem::path &path)
 {
@@ -73,4 +176,24 @@ usagi::node_graph::NodeGraph usagi::node_graph::NodeGraph::readFromFile(
 	}
 
 	return g;
+}
+
+std::tuple<usagi::Vector2f, usagi::Vector2f> usagi::node_graph::NodeGraphInstance::
+mapLinkEndPoints(std::size_t i) const
+{
+	auto [n0, p0, n1, p1] = base_graph->mapLink(i);
+	auto &l = base_graph->link(i);
+	auto pos0 = n0.prototype->portPosition(p0, node_positions[l.node0]);
+	auto pos1 = n1.prototype->portPosition(p1, node_positions[l.node1]);
+	return { pos0, pos1 };
+}
+
+usagi::AlignedBox2f usagi::node_graph::NodeGraphInstance::mapNodeRegion(
+	std::size_t node_index) const
+{
+	const auto node_position = mapNodePosition(node_index);
+	return {
+		node_position,
+		node_position + base_graph->node(node_index).prototype->size
+	};
 }
--- Graph/NodeGraph.hpp
+++ Graph/NodeGraph.hpp
@@ -23,13 +23,7 @@ struct Port
 	float edge_pos;
 
 	Port() = default;
-
-	Port(std::string name, Edge edge, float edge_pos)
-		: name(std::move(name))
-		, edge(edge)
-		, edge_pos(edge_pos)
-	{
-	}
+	Port(std::string name, Edge edge, float edge_pos);
 };
 
 struct NodePrototype
@@ -38,36 +32,17 @@ struct NodePrototype
 	Vector2f size;
 	std::vector<Port> out_ports, in_ports;
 
-	NodePrototype(std::string name, Vector2f size)
-		: name(std::move(name))
-		, size(std::move(size))
-	{
-	}
-
+	NodePrototype(std::string name, Vector2f size);
 	NodePrototype(
 		std::string name,
 		Vector2f size,
 		std::size_t in,
-		std::size_t out)
-		: NodePrototype(name, size)
-	{
-		createInPorts(in);
-		createOutPorts(out);
-	}
+		std::size_t out);
 
 	static void createPorts(
 		std::vector<Port> &sink,
 		std::size_t amount,
-		Port::Edge edge)
-	{
-		sink.resize(amount);
-		const float step = 1.f / (amount + 1);
-		for(std::size_t i = 0; i < amount; ++i)
-		{
-			sink[i].edge = edge;
-			sink[i].edge_pos = step * (i + 1);
-		}
-	}
+		Port::Edge edge);
 
 	void createOutPorts(std::size_t amount)
 	{
@@ -79,35 +54,7 @@ struct NodePrototype
 		createPorts(in_ports, amount, Port::Edge::WEST);
 	}
 
-	Vector2f portPosition(const Port &p, const Vector2f &position) const
-	{
-		Vector2f pos;
-		switch(p.edge)
-		{
-			case Port::Edge::NORTH:
-				pos.x() = lerp(p.edge_pos, 0.f, size.x())
-					+ position.x();
-				pos.y() = position.y();
-				break;
-			case Port::Edge::SOUTH:
-				pos.x() = lerp(p.edge_pos, 0.f, size.x())
-					+ position.x();
-				pos.y() = position.y() + size.y();
-				break;
-			case Port::Edge::WEST:
-				pos.x() = position.x();
-				pos.y() = lerp(p.edge_pos, 0.f, size.y())
-					+ position.y();
-				break;
-			case Port::Edge::EAST:
-				pos.x() = position.x() + size.x();
-				pos.y() = lerp(p.edge_pos, 0.f, size.y())
-					+ position.y();
-				break;
-			default: ;
-		}
-		return pos;
-	}
+	Vector2f portPosition(const Port &p, const Vector2f &position) const;
 
 	const Port & outPort(std::size_t i) const
 	{
@@ -125,11 +72,7 @@ struct Node
 	NodePrototype *prototype = nullptr;
 	std::string name;
 
-	Node(NodePrototype *prototype, std::string name)
-		: prototype(prototype)
-		, name(std::move(name))
-	{
-	}
+	Node(NodePrototype *prototype, std::string name);
 };
 
 struct Link
@@ -144,13 +87,7 @@ struct Link
 		std::size_t node0,
 		std::size_t port0,
 		std::size_t node1,
-		std::size_t port1)
-		: node0(node0)
-		, port0(port0)
-		, node1(node1)
-		, port1(port1)
-	{
-	}
+		std::size_t port1);
 };
 
 struct NodeGraph
@@ -171,48 +108,23 @@ struct NodeGraph
 	}
 
 	std::tuple<const Node&, const Port&, const Node&, const Port&>
-	mapLink(std::size_t i) const
-	{
-		auto &l = link(i);
-		auto &n0 = node(l.node0);
-		auto &n1 = node(l.node1);
-		auto &p0 = n0.prototype->outPort(l.port0);
-		auto &p1 = n1.prototype->inPort(l.port1);
-		return std::make_tuple(
-			std::ref(n0), std::ref(p0),
-			std::ref(n1), std::ref(p1)
-		);
-	}
+	mapLink(std::size_t i) const;
 
 	static NodeGraph readFromFile(const std::filesystem::path &path);
 };
 
-struct PortGraph
+struct NodeGraphInstance
 {
 	const NodeGraph *base_graph = nullptr;
 	Vector2f *node_positions = nullptr;
 
-	std::tuple<Vector2f, Vector2f> mapLinkEndPoints(std::size_t i) const
-	{
-		auto [n0, p0, n1, p1] = base_graph->mapLink(i);
-		auto &l = base_graph->link(i);
-		auto pos0 = n0.prototype->portPosition(p0, node_positions[l.node0]);
-		auto pos1 = n1.prototype->portPosition(p1, node_positions[l.node1]);
-		return { pos0, pos1 };
-	}
+	std::tuple<Vector2f, Vector2f> mapLinkEndPoints(std::size_t i) const;
 
 	Vector2f mapNodePosition(std::size_t node_index) const
 	{
 		return node_positions[node_index];
 	}
 
-	AlignedBox2f mapNodeRegion(std::size_t node_index) const
-	{
-		const auto node_position = mapNodePosition(node_index);
-		return {
-			node_position,
-			node_position + base_graph->node(node_index).prototype->size
-		};
-	}
+	AlignedBox2f mapNodeRegion(std::size_t node_index) const;
 };
 }
